20b11266227374fb0b519359ecdad7057057e39c
BenMing <ben.xmm86@gmail.com>
2019-02-18 16:14:23 +0800

    Update input_func.py

--- input_func.py
+++ input_func.py
@@ -11,16 +11,16 @@ def get_user_input(message, month_day_list):
         (str) message - 指示用户输入什么信息
         (str) month_day_list - 数据集中可查的月份或星期列表
     Returns:
-        (str) user_data - 用户输入
+        (str) user_input - 用户输入
     """
 
     while True:
-        user_data = input(message).lower()
-        if user_data in month_day_list:
+        user_input = input(message).lower()
+        if user_input in month_day_list:
             break
-        if user_data == 'all':
+        if user_input == 'all':
             break
         else:
             print("我没看懂你说的是什么。")
 
-    return user_data
+    return user_input
